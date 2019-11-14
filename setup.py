07ec2726e3c71cfd3f172a694ce825642116e4a1
Hui Shao <huiran.shao@sfgov.org>
2019-11-14 15:37:28 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-"""setup.py for boilerplate-sdk"""
+"""setup.py for accela-rest-sdk"""
 # pylint: disable=E0611,F0401
 #         No name 'core' in module 'distutils'
 #         Unable to import 'distutils.core'
 from distutils.core import setup
 import setuptools # pylint: disable=unused-import
-setup(name='boilerplate-sdk',
+setup(name='accela-rest-sdk',
       version='0.1.0',
       license='MIT',
       author='hshaosf',
       author_email='hshaosf@SFDigitalServices',
       url='https://github.com/SFDigitalServices',
-      packages=["boilerplate_sdk"],
+      packages=["accela_rest_sdk"],
       install_requires=[
           'requests'
       ]
